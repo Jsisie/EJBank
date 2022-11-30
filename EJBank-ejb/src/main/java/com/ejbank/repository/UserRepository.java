847f37aa7a92c20563373a991ff1860fc2d09c06
Léo Barroux <leo.barroux@gmail.com>
2022-11-30 09:32:43 +0100

    Defined UserRepo as a Bean to inject it

--- EJBank-ejb/src/main/java/com/ejbank/repository/UserRepository.java
+++ EJBank-ejb/src/main/java/com/ejbank/repository/UserRepository.java
@@ -2,9 +2,13 @@ package com.ejbank.repository;
 
 import com.ejbank.entity.UserEntity;
 
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+@Stateless
+@LocalBean
 public class UserRepository {
 
     @PersistenceContext(unitName = "EJBankPU")
