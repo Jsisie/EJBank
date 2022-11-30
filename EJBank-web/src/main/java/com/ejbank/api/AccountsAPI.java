cf3f792bcf310afbc9a8793bb330e4c29dcae61e
Léo Barroux <leo.barroux@gmail.com>
2022-11-30 11:45:42 +0100

    Typo error AccountAPI

--- EJBank-web/src/main/java/com/ejbank/api/AccountsAPI.java
+++ EJBank-web/src/main/java/com/ejbank/api/AccountsAPI.java
@@ -11,7 +11,7 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/acounts")
+@Path("/accounts")
 @Produces(MediaType.APPLICATION_JSON)
 @RequestScoped
 public class AccountsAPI {
