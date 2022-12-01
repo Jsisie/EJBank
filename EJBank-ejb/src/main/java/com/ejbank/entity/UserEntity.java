fdba71dae5ef0e170ae61dc6dd0e54b0d441dcf3
Léo Barroux <leo.barroux@gmail.com>
2022-12-01 11:43:33 +0100

    UserEntity finished

--- EJBank-ejb/src/main/java/com/ejbank/entity/UserEntity.java
+++ EJBank-ejb/src/main/java/com/ejbank/entity/UserEntity.java
@@ -10,34 +10,47 @@ public class UserEntity implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
-    @Column(name = "firstname", nullable =false, length = 20)
+    @Column(name = "login", nullable = false, length = 8)
+    private String login;
+    @Column(name = "password", nullable = false, length = 255)
+    private String password;
+    @Column(name = "email", nullable = false, length = 255)
+    private String email;
+    @Column(name = "firstname", nullable = false, length = 50)
     private String firstname;
-    @Column(name = "lastname", nullable =false, length = 20)
+    @Column(name = "lastname", nullable = false, length = 50)
     private String lastname;
+    @Column(name = "type", nullable = false, length = 50)
+    private String type;
 
-    public UserEntity() {}
-
-    public void setId(Integer id) {
-        this.id = id;
+    public UserEntity() {
     }
 
     public Integer getId() {
         return id;
     }
 
-    public String getLastname() {
-        return lastname;
+    public String getLogin() {
+        return login;
     }
 
-    public void setLastname(String lastName) {
-        this.lastname = lastName;
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
     }
 
     public String getFirstname() {
         return firstname;
     }
 
-    public void setFirstname(String firstName) {
-        this.firstname = firstName;
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getType() {
+        return type;
     }
 }
