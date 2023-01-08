ca6ef80df0883b18199a8e951b5473d0a64d0787
martin <martin.ricard@edu.univ-eiffel.com>
2023-01-08 10:58:05 +0100

    Added documentation for UserBeanServiceImpl.java

--- EJBank-ejb/src/main/java/com/ejbank/service/UserBean/UserBeanServiceImpl.java
+++ EJBank-ejb/src/main/java/com/ejbank/service/UserBean/UserBeanServiceImpl.java
@@ -17,6 +17,12 @@ public class UserBeanServiceImpl implements UserBeanService {
     public UserBeanServiceImpl() {
     }
 
+    /**
+     * Gets a first name and a last name from a user's id.
+     *
+     * @param id the id of the user. (int)
+     * @return The first name and the last name of a user as a UserPayload. (UserPayload)
+     */
     @Override
     public UserPayload getName(int id) {
         return userRepository.getName(id);
