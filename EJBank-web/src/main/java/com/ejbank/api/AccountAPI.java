dcc0882f89324d0ba738df9ccc03132e50654b8c
martin <martin.ricard@edu.univ-eiffel.com>
2023-01-08 11:32:29 +0100

    Added documentation for AccountAPI.

--- EJBank-web/src/main/java/com/ejbank/api/AccountAPI.java
+++ EJBank-web/src/main/java/com/ejbank/api/AccountAPI.java
@@ -18,12 +18,29 @@ public class AccountAPI {
     @EJB
     private AccountBeanServiceImpl accountBeanService;
 
+    /**
+     * Test method
+     * Returns a test String.
+     *
+     * @return A test String "test Account". (String)
+     */
     @GET
     @Path("/test")
     public String testEJBFirstName() {
         return accountBeanService.test();
     }
 
+    /**
+     * Returns a user's account as an AccountPayload.
+     *
+     * @param accountID The id of the account that is going to be searched for. (Integer)
+     * @param userID The id of the supposed account's owner. (Integer)
+     * @return The specified account for the specified user as an AccountPayload. Returns an error AccountPayload if :
+     *      - The given ID does not match with any user's.
+     *      - The user is an advisor and the account  isn't part of his supervised accounts.
+     *      - The user is a customer and the account doesnt belong to it.
+     *      (AccountPayload)
+     */
     @GET
     @Path("/{account_id}/{user_id}")
     public AccountPayload getAccount(@PathParam("account_id") Integer accountID, @PathParam("user_id") Integer userID) {
