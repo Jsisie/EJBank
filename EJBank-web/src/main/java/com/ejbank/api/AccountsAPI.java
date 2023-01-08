ec1d99874227c49180aa3ba159fe0798044fc881
martin <martin.ricard@edu.univ-eiffel.com>
2023-01-08 11:35:14 +0100

    Added documentation for AccountsAPI.

--- EJBank-web/src/main/java/com/ejbank/api/AccountsAPI.java
+++ EJBank-web/src/main/java/com/ejbank/api/AccountsAPI.java
@@ -18,24 +18,53 @@ public class AccountsAPI {
     @EJB
     private AccountsBeanServiceImpl accountsBeanService;
 
+    /**
+     * Test method
+     * Returns a test String.
+     *
+     * @return A test String "test Account". (String)
+     */
     @GET
     @Path("/test")
     public String testEJBFirstName() {
         return accountsBeanService.test();
     }
 
+    /**
+     * Gathers all the accounts of a customer and returns them as a ListAccountsPayload.
+     *
+     * @param id The id of the user (customer) to retrieve the accounts from. (Integer)
+     * @return A ListAccountsPayload of the customer's accounts, or "The User is not a customer" if the ID redirects
+     * to an advisor (ListAccountPayload).
+     */
     @GET
     @Path("/{user_id}")
     public ListAccountsPayload getAccountsFromUserId(@PathParam("user_id") Integer id) {
         return accountsBeanService.getAccounts(id);
     }
 
+    /**
+     * Returns all the customer accounts an advisor has access to as a ListAccountsPayload. If the id is the one of a
+     * customer, returns "The User is not an advisor" as a response.
+     *
+     * @param id The id of the user (advisor) to retrieve the customer accounts from. (Integer)
+     * @return A ListAccountsPayload of containing an advisor's attached customer accounts. Or a ListAccountsPayload with
+     * the message "The User is not an advisor" if the given id is one of a customer.
+     */
     @GET
     @Path("/attached/{user_id}")
     public ListAccountsPayload getAttachedAccountsFromUserId(@PathParam("user_id") Integer id) {
         return accountsBeanService.getAttachedAccounts(id);
     }
 
+    /**
+     * If the given ID is one of an advisor : returns a ListAccountsPayload of its attached customer accounts.
+     * Else, returns a ListAccountsPayload of all the accounts of the given customer.
+     *
+     * @param id The user we want to get the accounts or the attached accounts from. (Integer)
+     * @return A ListAccountsPayload of all the advisor's attached customers accounts, or all the customer's account.
+     * (ListAccountsPayload)
+     */
     @GET
     @Path("/all/{user_id}")
     public ListAccountsPayload getAllAccountsFromUserId(@PathParam("user_id") Integer id) {
